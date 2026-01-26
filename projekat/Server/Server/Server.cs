4ef56ce70a514b1b006619e796db612c9a0020b8
belanovpedja <belanovpedja@gmail.com>
2026-01-26 18:56:07 +0100

    Projekta folder upload

---
+++ projekat/Server/Server/Server.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using PomocneKlase;
+
+namespace Server
+{
+    internal class Server
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
