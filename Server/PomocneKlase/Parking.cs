01783712853df63592fd802b806e3eb5a728fef1
Pedja <s7pedja@gmail.com>
2026-01-27 14:26:41 +0100

    Client i klase

--- Server/PomocneKlase/Parking.cs
+++ Server/PomocneKlase/Parking.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PomocneKlase
 {
-    internal class Parking
+    //NE TREBA SERIALIZABLE NE SALJEM GA
+    public class Parking
     {
+
+        public int BrojMesta { get; set; }
+        public int BrojZauzetih { get; set; }
+
+        public double Cena { get; set; }
+
+        public Parking(int brojMesta, int brojZauzetih, double cena)
+        {
+            BrojMesta = brojMesta;
+            BrojZauzetih = brojZauzetih;
+            Cena = cena;
+        }
 
     }
 }
