30f46a17aeae05f622280f139979b397a1b1dfce
Pedja <s7pedja@gmail.com>
2026-01-27 15:19:22 +0100

    Client i klase

---
+++ PomocneKlase/Parking.cs
@@ -0,0 +1,19 @@
+﻿namespace PomocneKlase
+{
+    //NE TREBA SERIALIZABLE NE SALJEM GA
+    public class Parking
+    {
+        public int BrojMesta { get; set; }
+        public int BrojZauzetih { get; set; }
+
+        public double Cena { get; set; }
+
+        public Parking(int brojMesta, int brojZauzetih, double cena)
+        {
+            BrojMesta = brojMesta;
+            BrojZauzetih = brojZauzetih;
+            Cena = cena;
+        }
+
+    }
+}
--- Server/Server/Server.cs
+++ Server/Server/Server.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-    internal class Program
+    internal class Server
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("aaa");
         }
     }
 }
